9d665438f3ad48f48530269d6e3493dc185b7ec1
Simon <simon@Nannou.(none)>
2012-09-16 11:25:33 +0100

    customdictionary now works as a class

--- customdictionary.py
+++ customdictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
- 
 import pickle
  
 class UnknownWordException(Exception):
@@ -10,9 +9,11 @@ class UnknownWordException(Exception):
 
 class CustomDictionary(object):
   def __init__(self):
+    object.__init__(self)
+    self.__unknown_words = []
     try:
       dictionary_file = open("customdictionary", "r")
-      self.dictionary = pickle.load(dictionary_file)
+      self.__dictionary = pickle.load(dictionary_file)
       dictionary_file.close()
     except IOError as e:
       debug("Failed to open/read from dictionary file: " + str(e))
@@ -21,19 +22,27 @@ class CustomDictionary(object):
   def __del__(self):
     try:
       dictionary_file = open("customdictionary", "w")
-      pickle.dump(self.dictionary, dictionary_file)
+      pickle.dump(self.__dictionary, dictionary_file)
       dictionary_file.close()
+      
+      unknownwords_file = open("unknownwords", "w")
+      pickle.dump(self.__unknown_words, unknownwords_file)
+      unknownwords_file.close()
     except IOError as e:
       debug("Failed to save to dictionary file: " + str(e))
       
   def get_nsyl(self, word):
     word = word.lower()
     try:
-      return self.dictionary[word]
+      return self.__dictionary[word]
     except KeyError as e:	
-      #unknownWordsList += [word]
+      self.__unknown_words += [word]
       raise UnknownWordException(word);
 
 if __name__=="__main__":
   customdictionary = CustomDictionary()
-  print customdictionary.get_nsyl("bredon")
+  try:
+    print customdictionary.get_nsyl("bredon")
+    print customdictionary.get_nsyl("bredons")
+  except UnknownWordException as e:
+    print "Unknown Word: " + str(e)
