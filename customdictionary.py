1790b51feaf2c3fba1bb7af4ad8bd29ed0715b17
Simon <simon@Nannou.(none)>
2012-09-16 10:05:49 +0100

    clean custom dictionary

--- customdictionary.py
+++ customdictionary.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python
 
-#TODO:Rewrite this dictionary stuff, use a custom class
 unknownWordsList = []
-#unknownMathList = []
 customDictionary = {"abelian":3, "bredon":2, "homology":4, "homotopy":4, "functor":2, "functors":2, "finiteness":3, "organising":4, "homological":5, "quotients":2, "pointwise":2, "familiarised":4, "pro-finite":3, "whitecaps":2, "signboard":2}
 
 class UnknownWordException(Exception):
-       def __init__(self, value):
-           self.word = value
-       def __str__(self):
-           return repr(self.word)
+  def __init__(self, value):
+   self.word = value
+  def __str__(self):
+    return repr(self.word)
 
 def get_nsyl_from_custom_dict(word):
-      global unknownWordsList 
-      try:
-	return customDictionary[word]
-      except KeyError as e:	
-	unknownWordsList += [word]
-	raise UnknownWordException(word);
+  global unknownWordsList 
+  try:
+    return customDictionary[word]
+  except KeyError as e:	
+    unknownWordsList += [word]
+    raise UnknownWordException(word);
       
 if __name__=="__main__":
   print "No tests here yet.."
