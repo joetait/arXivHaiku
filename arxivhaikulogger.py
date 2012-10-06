560ea76868b923588b8579c138a6b34c216de005
Simon <simon@Nannou.(none)>
2012-10-06 17:00:49 +0100

    Change log level to INFO

--- arxivhaikulogger.py
+++ arxivhaikulogger.py
@@ -3,7 +3,7 @@
 import logging
 
 def setup_custom_logger(name, log_file_name="arXivHaiku.log"):
-    DEFAULT_LOG_LEVEL = logging.DEBUG #Change to debug for more messages
+    DEFAULT_LOG_LEVEL = logging.INFO #Change to debug for more messages
   
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     handler = logging.FileHandler(log_file_name)
