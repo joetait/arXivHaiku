b13181cc946e24867415e2853e7a18dab4e0eddd
Simon(Nannou) <Simon.StJG@gmail.com>
2013-08-21 14:43:38 +0100

    printlicence

--- facebookintegration.py
+++ facebookintegration.py
@@ -19,6 +19,14 @@
 
 import facebook, logging
 
+def printlicense():
+  print """
+  findhaiku is part of arXivHaiku  Copyright 2012 Simon StJohn-Green
+    This program comes with ABSOLUTELY NO WARRANTY; for details see gpl.txt
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; see gpl.txt for details.
+  """     
+
 if __name__!="__main__":
   logger = logging.getLogger('mainLogger')
 
@@ -49,6 +57,7 @@ def post_to_facebook(arxivhaiku_graph, message, backdated_time=None):
    
 if __name__=="__main__":
   import arXivHaikulogger
+  printlicense()
   logger = arXivHaikulogger.setup_custom_logger('mainLogger')  #No need for global here - already at global scope
   logger.info("Running facebookintegration with __name__==__main__: ")
   personal_access_token = 'CAACEdEose0cBAFz6OC0rVqQKhqowjVVVeQhOvCB1etQnPM5ddpxmr3oCoZARGZAzrHzR8ZClCKDmCZBLHcg0mONvvyt9BhQuN5P8C0VGOwioHhvXmoHBRcQOW0I0S4EC9U9vZB54OpO4nx6auYDguFAaz3i6xHZBbgZAHZCm5Ic9zAZDZD'
