0fb8430ee2f0118e0f928902cde9be3c57b79a50
Simon(Nannou) <Simon.StJG@gmail.com>
2013-08-21 14:23:26 +0100

    logging in facebookintegration

--- facebookintegration.py
+++ facebookintegration.py
@@ -17,29 +17,40 @@
 #You should have received a copy of the GNU General Public License
 #along with arXivHaiku.  If not, see <http://www.gnu.org/licenses/>.
 
-import facebook
+import facebook, logging
+
+if __name__!="__main__":
+  logger = logging.getLogger('mainLogger')
 
 def get_arxivhaiku_graph(personal_access_token):
+  logger.info("Attempting to get arxivhaiku graph from facebook...")
   try:
     personal_graph = facebook.GraphAPI(personal_access_token)
     arxivhaiku_access_token = personal_graph.get_object("Arxivhaiku", fields="access_token")["access_token"]
-    return facebook.GraphAPI(arxivhaiku_access_token)
+    arxivhaiku_graph = facebook.GraphAPI(arxivhaiku_access_token)
+    logger.info("Successfully got arxivhaiku graph")
+    return arxivhaiku_graph
   except facebook.GraphAPIError as e:
-    print "post-to-facebook caught GraphAPIError: " + str(e)
+    logger.warning("post-to-facebook caught GraphAPIError: " + str(e))
     return False
 
 def post_to_facebook(arxivhaiku_graph, message, backdated_time=None):
+  logger.info("Attempting to post to facebook: " + message)
   try:
     if backdated_time==None:
       arxivhaiku_graph.put_object("Arxivhaiku", "feed", message=message)
     else:
       arxivhaiku_graph.put_object("Arxivhaiku", "feed", message=message, backdated_time=backdated_time)
+    logger.info("Post successful.")
     return True
   except facebook.GraphAPIError as e:
-    print "post-to-facebook caught GraphAPIError: " + str(e)
+    logger.warning("Post unsuccessful, caught GraphAPIError: " + str(e))
     return False
    
 if __name__=="__main__":
+  import arXivHaikulogger
+  logger = arXivHaikulogger.setup_custom_logger('mainLogger')  #No need for global here - already at global scope
+  logger.info("Running facebookintegration with __name__==__main__: ")
   personal_access_token = 'CAACEdEose0cBAFz6OC0rVqQKhqowjVVVeQhOvCB1etQnPM5ddpxmr3oCoZARGZAzrHzR8ZClCKDmCZBLHcg0mONvvyt9BhQuN5P8C0VGOwioHhvXmoHBRcQOW0I0S4EC9U9vZB54OpO4nx6auYDguFAaz3i6xHZBbgZAHZCm5Ic9zAZDZD'
   arxivhaiku_graph = get_arxivhaiku_graph(personal_access_token)
   message = "That we shall discuss (very superficially) in the next chapter.  (1211.5627) #arXivHaiku"
